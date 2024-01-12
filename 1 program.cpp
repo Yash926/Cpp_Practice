e9b5a483fc28748363da62cba10ca91cfa6e0631
Yash Tripathi <114800027+Yash926@users.noreply.github.com>
2024-01-12 22:48:57 +0530

    Basic CPP

---
+++ 1 program.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	int b;
+	int add;
+	cout<<"enter first number:";
+	cin>>a;
+	cout<<"enter 2 number:";
+	cin>>b;
+	cout<<"Sum of two numbers is:"<<a+b;
+}
+
